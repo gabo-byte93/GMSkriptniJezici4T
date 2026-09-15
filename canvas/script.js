30cdb515f5837d4824d66afb9acb194a875cf52a
gabo-byte93 <milakgabriel93@gmail.com>
2026-09-15 13:09:22 +0200

    Add files via upload

--- canvas/script.js
+++ canvas/script.js
@@ -27,3 +27,67 @@ ctx.fillRect(200,100,50,50);
 
 ctx.fillStyle ="red";
 ctx.fillRect(300,150,50,50);
+
+/* crtanje linije */
+/* ova metoda zepocinje novu putanju */
+ctx.beginPath();
+
+/* postavlja pocetnu poziciju */
+ctx.moveTo(50,50);
+
+/* definira liniju od pocetne pozicije do zadane tocke */
+ctx.lineTo(300,200);
+
+/* Iscrtava putanju */ 
+ctx.stroke();
+
+/* Sad radimo vise povezanih crta */
+ctx.beginPath();
+ctx.moveTo(100,300);
+ctx.lineTo(200,200);
+ctx.lineTo(300,300);
+
+/* Zatvara putanju */
+ctx.closePath();
+
+ctx.fillStyle="red";
+ctx.fill();
+
+ctx.strokeStyle="purple";
+ctx.stroke();
+
+/* Crtanje kruga */
+
+ctx.beginPath();
+
+/* Metoda arc sluzi za crtanje kruga */
+
+    /* 300 je x koordinate, 200 je y koordinata, 80 polumjer, 0 početni kut, 2*Math.PI, predstavlja završni kut*/
+ctx.arc(300,200,80,0,2*Math.PI);
+
+ctx.fillStyle="purple";
+ctx.fill();
+
+ctx.strokeStyle="cyan";
+ctx.stroke();
+
+/* Polukrug */
+/* Za polukrug je sintaksa identicna samo umjesto 2*Math.PI stavimo Math.Pi */
+
+ctx.beginPath();
+ctx.arc(500,300,80,0,Math.PI);
+ctx.strokeStyle="Brown";
+ctx.stroke();
+
+ctx.fillStyle="darkblue";
+ctx.fill();
+
+/* Dodavanje teksta */
+/* Odreduje velicinu i izgled teksta */
+ctx.font = "30px Arial";
+ctx.fillStyle = "lime";
+
+/* Koristi se za ispis teksta na canvasu */
+ctx.fillText("Karlo Benjak",250,250);
+
+
